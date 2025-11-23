f447d53c7637d1583465c910c20a1352da3e317f
MrMangoye <benjaminjuma512@gmail.com>
2025-11-23 20:55:37 +0300

    update

--- Client/Index.js
+++ Client/Index.js
@@ -7,13 +7,4 @@ mongoose.connect(process.env.MONGO_URI)
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
   })
-  .catch(err => console.error('❌ MongoDB connection error:', err));import React from "react";
-  import ReactDOM from "react-dom/client";
-  import App from "./App";
-  import "./index.css";
-  
-  ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+  .catch(err => console.error('❌ MongoDB connection error:', err));
